15479ecde7f98fad86a14e30a52878e89c9ebd94
KFE-LC <kfemelue@gmail.com>
2019-10-31 19:27:31 -0500

    assignment 6 step 8 complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,13 @@ import {Satellite} from './satellite';
 export class AppComponent {
   title = 'orbit-report';
   sourceList: Satellite[];
+  displayList: Satellite[];
 
 //constructor to fetch json data to populate table
 constructor() {
   this.sourceList = [];
   let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
-
+  this.displayList=[];
   window.fetch(satellitesUrl).then(function(response) {
      response.json().then(function(data) {
 
@@ -33,10 +34,30 @@ constructor() {
         };
         
         
-
+        
+        this.displayList = this.sourceList.slice(0);
      }.bind(this));
   }.bind(this));
 
+
+
+  
+
+  
+
+}
+search(searchTerm: string): void {
+  let matchingSatellites: Satellite[] = [];
+  searchTerm = searchTerm.toLowerCase();
+  for(let i=0; i < this.sourceList.length; i++) {
+     let name = this.sourceList[i].name.toLowerCase();
+     if (name.indexOf(searchTerm) >= 0) {
+        matchingSatellites.push(this.sourceList[i]);
+     }
+  }
+  // assign this.displayList to be the the array of matching satellites
+  // this will cause Angular to re-make the table, but now only containing matches
+  this.displayList = matchingSatellites;
 }
 
 
